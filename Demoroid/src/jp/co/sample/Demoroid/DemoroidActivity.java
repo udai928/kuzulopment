7ceae84e067a531f758b846146397b0845a8e490
葛屋 雄大 <udai928@gmail.com>
2013-10-20 21:16:35 +0900

    making kuzuroid

---
+++ Demoroid/src/jp/co/sample/Demoroid/DemoroidActivity.java
@@ -0,0 +1,25 @@
+package jp.co.sample.Demoroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DemoroidActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	android.util.Log.v("タグ１","開始メッセージ");
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demoroid);
+        android.util.Log.v("タグ２","終了メッセージ");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.demoroid, menu);
+        return true;
+    }
+    
+}
