740fa2555086fb7876a44cf1614dd7bb74d50676
葛屋 雄大 <udai928@gmail.com>
2013-10-22 18:15:51 +0900

    commit1022-18:15-7-15

--- GuiSample/src/jp/co/sample/GuiSample/GuiSampleActivity.java
+++ GuiSample/src/jp/co/sample/GuiSample/GuiSampleActivity.java
@@ -4,6 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class GuiSampleActivity extends Activity {
 
@@ -11,6 +22,35 @@ public class GuiSampleActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gui_sample);
+        Button button =(Button)findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener(){
+        	public void onClick(View v){
+        		EditText edit = (EditText)findViewById(R.id.editText1);
+        		if (edit.getText().length() == 0){
+        				TextView text = (TextView)findViewById(R.id.textView1);
+        				text.setText("Error input user");
+        		}
+        	}
+        });
+        button.setEnabled(false);
+        EditText edit = (EditText)findViewById(R.id.editText1);
+		edit.setOnKeyListener(new OnKeyListener(){
+			public boolean onKey(View v, int keyCode, KeyEvent event){
+				Button button = (Button)findViewById(R.id.button1);
+				button.setEnabled(((EditText)v).getText().length() > 0 ? true : false);
+				return false;
+			}		
+		});
+		
+		LinearLayout layout = (LinearLayout)findViewById(R.id.LinearLayout2);
+		CheckBox check = new CheckBox(this);
+		check.setText("Remember user and password");
+		check.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+				android.util.Log.v("OnCheckedChanged","check " + isChecked);
+			}
+		});
+		layout.addView(check);
     }
 
 
