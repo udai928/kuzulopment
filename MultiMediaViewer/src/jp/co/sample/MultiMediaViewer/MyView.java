d424878f2c75335f5b2161b7280a06009fbe79b9
葛屋 雄大 <udai928@gmail.com>
2013-10-23 15:25:04 +0900

    commit1023-15:24-9-11

--- MultiMediaViewer/src/jp/co/sample/MultiMediaViewer/MyView.java
+++ MultiMediaViewer/src/jp/co/sample/MultiMediaViewer/MyView.java
@@ -1,7 +1,9 @@
 package jp.co.sample.MultiMediaViewer;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View{
@@ -9,6 +11,22 @@ public class MyView extends View{
 	public MyView(Context context) {
 		super(context);
 		setBackgroundColor(Color.LTGRAY);
+	}
+	@Override
+	protected void onDraw(Canvas canvas){
+		Paint paint = new Paint();
+		paint.setColor(Color.BLACK);
+		paint.setTextSize(48);
+		paint.setAntiAlias(true);
+		canvas.drawText("MyView Test", 20, 50, paint);
+		paint.setColor(Color.RED);
+		paint.setStrokeWidth(3);
+		canvas.drawLine(0,  0,  100, 100, paint);
+		paint.setColor(Color.BLUE);
+		canvas.drawRect(0, 100, 200, 200, paint);
+		paint.setColor(Color.GREEN);
+		paint.setStyle(Paint.Style.STROKE);
+		canvas.drawRect(0,  200, 300, 300, paint);
 	}
 
 }
