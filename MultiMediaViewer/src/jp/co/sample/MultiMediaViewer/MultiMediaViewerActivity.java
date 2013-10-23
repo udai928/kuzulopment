da9a3ad9108137f6cc00123e756698b0c5ca1b98
葛屋 雄大 <udai928@gmail.com>
2013-10-23 18:26:19 +0900

    commit!!1023-18:26-11-1

--- MultiMediaViewer/src/jp/co/sample/MultiMediaViewer/MultiMediaViewerActivity.java
+++ MultiMediaViewer/src/jp/co/sample/MultiMediaViewer/MultiMediaViewerActivity.java
@@ -2,14 +2,26 @@ package jp.co.sample.MultiMediaViewer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 
 public class MultiMediaViewerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+        MyView view = new MyView(this);
+        setContentView(view);
+        AnimationSet animation = new AnimationSet(true);
+        animation.addAnimation(new RotateAnimation(0, 360, 300, 400));
+        animation.addAnimation(new ScaleAnimation(0.4f, 2, 0.4f, 2, 300, 400));
+        animation.setDuration(4000);
+        view.startAnimation(animation);
     }
 
 
@@ -20,4 +32,25 @@ public class MultiMediaViewerActivity extends Activity {
         return true;
     }
     
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent e){
+    	if(keyCode == KeyEvent.KEYCODE_BACK){
+    		new AlertDialog.Builder(this)
+    			.setTitle("終了の確認")
+    			.setMessage("本当に終了しますか")
+    			.setPositiveButton("はい", new DialogInterface.OnClickListener(){
+    				public void onClick(DialogInterface dialog, int which){
+    						finish();
+    				}
+    			})
+    			.setNegativeButton("いいえ", new DialogInterface.OnClickListener(){
+    				public void onClick(DialogInterface dialog, int which){
+    				}
+    			})
+    			.show();
+    		return true;
+    	}
+    	else
+    		return false;
+    }
 }
--- MultiMediaViewer/src/jp/co/sample/MultiMediaViewer/MyView.java
+++ MultiMediaViewer/src/jp/co/sample/MultiMediaViewer/MyView.java
@@ -1,19 +1,40 @@
 package jp.co.sample.MultiMediaViewer;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 
-public class MyView extends View{
-
+public class MyView extends View implements GestureDetector.OnGestureListener,
+	ScaleGestureDetector.OnScaleGestureListener{
+	private GestureDetector gestureDetector = null;
+	private ScaleGestureDetector scaleGestureDetector = null;
+	private Bitmap bitmap = null;
+	private float x = 0f;
+	private float y = 0f;
+	private float scale = 1f;
+	
 	public MyView(Context context) {
 		super(context);
 		setBackgroundColor(Color.LTGRAY);
+		bitmap = BitmapFactory.decodeResource(context.getResources(),  R.drawable.ic_launcher);
+		gestureDetector = new GestureDetector(context, this);
+		scaleGestureDetector = new ScaleGestureDetector(context, this);
 	}
 	@Override
 	protected void onDraw(Canvas canvas){
+		canvas.translate(x, y);
+		canvas.rotate(-20);
+		canvas.scale(scale,  scale);
+		canvas.scale(0.8f,  0.8f);
 		Paint paint = new Paint();
 		paint.setColor(Color.BLACK);
 		paint.setTextSize(48);
@@ -27,6 +48,64 @@ public class MyView extends View{
 		paint.setColor(Color.GREEN);
 		paint.setStyle(Paint.Style.STROKE);
 		canvas.drawRect(0,  200, 300, 300, paint);
+		canvas.drawBitmap(bitmap, 20, 300, null);
 	}
-
+	@Override
+	public boolean onTouchEvent(MotionEvent e){
+		if(e.getPointerCount() > 1){
+			scaleGestureDetector.onTouchEvent(e);
+			return true;
+		}
+		gestureDetector.onTouchEvent(e);
+		return true;	
+	}
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+	@Override
+	public void onLongPress(MotionEvent e) {
+		Animation animation = new ScaleAnimation(1, 3, 1, 3, x, y);
+		animation.setDuration(2000);
+		startAnimation(animation);
+	}
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+		x -= distanceX;
+		y -= distanceY;
+		invalidate();
+		return true;
+	}
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+	@Override
+	public boolean onScale(ScaleGestureDetector detector) {
+		scale *= detector.getScaleFactor();
+		invalidate();
+		return false;
+	}
+	@Override
+	public boolean onScaleBegin(ScaleGestureDetector detector) {
+		return true;
+	}
+	@Override
+	public void onScaleEnd(ScaleGestureDetector detector) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
 }
