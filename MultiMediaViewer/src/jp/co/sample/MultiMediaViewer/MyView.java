20aed856b9d5b7a413f248221d75a323d8b8874e
葛屋 雄大 <udai928@gmail.com>
2013-10-23 14:38:33 +0900

    commit1023-14:38-9-4

---
+++ MultiMediaViewer/src/jp/co/sample/MultiMediaViewer/MyView.java
@@ -0,0 +1,14 @@
+package jp.co.sample.MultiMediaViewer;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+
+public class MyView extends View{
+
+	public MyView(Context context) {
+		super(context);
+		setBackgroundColor(Color.LTGRAY);
+	}
+
+}
