452a7e775516b83ed1d3403e4a637b2a649fd560
葛屋 雄大 <udai928@gmail.com>
2013-10-31 18:05:36 +0900

    commit1031-18:05 screampic 表示まで

--- ScreamGame2/src/jp/example/screamgame2/GameActivity.java
+++ ScreamGame2/src/jp/example/screamgame2/GameActivity.java
@@ -4,30 +4,36 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Region.Op;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
-import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class GameActivity extends Activity {
-	private changeView right = null;
-
+	private changeView1 right1 = null;
+	private changeView2 right2 = null;
+	private Bitmap resize_pic, pic;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		FrameLayout frameLayout = new FrameLayout(this);
 		frameLayout.setBackgroundColor(Color.argb(255,255,153,204));
 		setContentView(frameLayout);
-		
-	//	requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
+
+//		requestWindowFeature(Window.FEATURE_NO_TITLE);
+//		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//		
 		ImageView iv = new ImageView(this);
 		iv.setImageResource(R.drawable.screamgame1_3);
 		addContentView(iv, new LayoutParams(490, 700));
@@ -38,17 +44,52 @@ public class GameActivity extends Activity {
             Toast.makeText(this, str, Toast.LENGTH_LONG).show();
 		}
 	}
-	class changeView extends View{
-		private Paint paint = new Paint();
-	
-		public changeView(Context context){
-			super(context);
+	class changeView1 extends View{
+		private Paint paint1 = new Paint();
+		private Paint paint2 = new Paint();
+		private Path clipPath1 = new Path();
+		private Path clipPath2 = new Path();
+
+		public changeView1(Context context1){
+			super(context1);
 		}
-		public void onDraw(Canvas canvas) {
-		    Paint paint = new Paint();
-		    paint.setColor(Color.argb(255, 255, 255, 255));
-		    paint.setAntiAlias(true);
-		    canvas.drawCircle(128, 280, 30, paint);
+		public void onDraw(Canvas canvas1) {
+			clipPath1.addCircle(130, 280, 25, Path.Direction.CW);
+		    canvas1.clipPath(clipPath1, Op.DIFFERENCE);
+		    paint1.setColor(Color.RED);
+		    paint1.setAntiAlias(true);
+		    canvas1.drawCircle(130, 280, 30, paint1);
+		    
+		    clipPath2.addCircle(377, 280, 25, Path.Direction.CW);
+		    canvas1.clipPath(clipPath2, Op.DIFFERENCE);
+		    paint2.setColor(Color.RED);
+		    paint2.setAntiAlias(true);
+		    canvas1.drawCircle(377, 280, 30, paint2);
+		    
+		}
+	}
+	class changeView2 extends changeView1{
+		private Paint paint3 = new Paint();
+		private Paint paint4 = new Paint();
+		private Path clipPath3 = new Path();
+		private Path clipPath4 = new Path();
+		
+		public changeView2(Context context2){
+			super(context2);
+		}
+		public void onDraw(Canvas canvas2) {
+			clipPath3.addCircle(145, 454, 25, Path.Direction.CW);
+		    canvas2.clipPath(clipPath3, Op.DIFFERENCE);
+		    paint3.setColor(Color.RED);
+		    paint3.setAntiAlias(true);
+		    canvas2.drawCircle(145, 454, 30, paint3);
+		    
+		    clipPath4.addCircle(390, 454, 25, Path.Direction.CW);
+		    canvas2.clipPath(clipPath4, Op.DIFFERENCE);
+		    paint4.setColor(Color.RED);
+		    paint4.setAntiAlias(true);
+		    canvas2.drawCircle(390, 454, 30, paint4);
+		    
 		}
 	}
 	@Override
@@ -58,25 +99,32 @@ public class GameActivity extends Activity {
 	}
 	@Override
 	public boolean onTouchEvent(MotionEvent event){ 
-		if((event.getX()>115 && event.getX()<141)&&(event.getY()>376 && event.getY()<400)){
-			Toast.makeText(this, "right!!", Toast.LENGTH_LONG).show();
-			right = new changeView(this);
-			addContentView(right, new LayoutParams(360, 388));
+		if((event.getX()>110 && event.getX()<146)&&(event.getY()>380 && event.getY()<405) || 
+				(event.getX()>348 && event.getX()<381)&&(event.getY()>380 && event.getY()<405)){
+			Toast.makeText(this, "right1!!", Toast.LENGTH_SHORT).show();
+			right1 = new changeView1(this);
+			addContentView(right1, new LayoutParams(500, 500));
+			//カメラ起動開始
+			
 		}
-		if((event.getX()>352 && event.getX()<376)&&(event.getY()>376 && event.getY()<400)){
-			Toast.makeText(this, "right!!", Toast.LENGTH_LONG).show();
+		if((event.getX()>125 && event.getX()<165)&&(event.getY()>540 && event.getY()<563)||
+				(event.getX()>385 && event.getX()<405)&&(event.getY()>540 && event.getY()<563)){
+			//Toast.makeText(this, "right2!!", Toast.LENGTH_SHORT).show();
+			//right2 = new changeView2(this);
+			//addContentView(right2, new LayoutParams(500, 700));//表示範囲（）
+			//画像表示、音声
+			
+			pic = BitmapFactory.decodeResource(getResources(), R.drawable.screampic1_2);
+			resize_pic = Bitmap.createScaledBitmap(pic, 100, 100, true);
+			ImageView img = new ImageView(this);
+			img.setImageBitmap(resize_pic);
+			setContentView(img, new LayoutParams(700, 700));
 		}
 		
-		if((event.getX()>130 && event.getX()<152)&&(event.getY()>547 && event.getY()<557)){
-			Toast.makeText(this, "right!!", Toast.LENGTH_LONG).show();
-		}
-		if((event.getX()>380 && event.getX()<400)&&(event.getY()>547 && event.getY()<557)){
-			Toast.makeText(this, "right!!", Toast.LENGTH_LONG).show();
-		}
-
 		 android.util.Log.v("MotionEvent",
 			        "x = " + String.valueOf(event.getX()) + ", " +
 			        "y = " + String.valueOf(event.getY()));
 		return true;
 	}
+	
 }
--- ScreamGame2/src/jp/example/screamgame2/MainActivity.java
+++ ScreamGame2/src/jp/example/screamgame2/MainActivity.java
@@ -3,25 +3,27 @@ package jp.example.screamgame2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	    requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        FrameLayout frameLayout = new FrameLayout(this);
+		frameLayout.setBackgroundColor(Color.argb(255,255,255,100));
+		setContentView(frameLayout);
+		View view =  getLayoutInflater().inflate(R.layout.activity_main, null);
+		addContentView(view,new LayoutParams(490, 700));
         Button btn = (Button)findViewById(R.id.button1);
 		btn.setOnClickListener(this);
     }
-   
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -32,7 +34,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		Intent intent = new Intent();
 		intent.setClassName("jp.example.screamgame2","jp.example.screamgame2.GameActivity");
-		intent.putExtra("jp.example.screamgame2.GameActivity", "main game running");
+		intent.putExtra("jp.example.screamgame2.GameActivity", "find three maistakes in the picture!");
 		startActivity(intent);
 		android.util.Log.v("running!!!!!!!!!!!!!!!!!!", "a");
 	}    
